adc758c1e662486c6bf07eba51721755b97ba029
JOHN <johnmickey281@gmail.com>
2020-08-18 12:21:19 -0500

    Add project files.

---
+++ CONSOLEAPP .NETCORE/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Channels;
+
+namespace DotNetCoreConsole
+{
+    class Program
+    {
+        static void SayHi()
+        {
+            Console.WriteLine("hi");
+        }
+
+        static void Main(string[] args)
+        {
+
+            SayHi();
+
+        }
+    }
+}
---
+++ Methods102/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Methods102
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          //  GetFirstName();
+            Myfunction();
+        }
+
+        static void Myfunction()
+
+        {
+            Console.WriteLine("this is our function getting executed");
+        }
+        static string GetFirstName()
+        {
+            string Firstname = "";
+            Console.WriteLine("enter first name:\n");
+            Firstname = Console.ReadLine();
+            return Firstname;
+        }
+    }
+}
